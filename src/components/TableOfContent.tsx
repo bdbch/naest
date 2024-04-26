ae9120c099dc146ebf410cee77990c56e5c9862f
bdbch <github@bdbch.mozmail.com>
2024-04-27 00:30:17 +0200

    add active states for ToC

--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import { cn } from "@/utils/cn";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
+const ToCItem = ({
+  id,
+  title,
+  active,
+}: {
+  id: string;
+  title: string;
+  active: boolean;
+}) => {
+  const linkClassName = cn(
+    "text-sm block p-2 border-l border-l-neutral-800 transition-colors",
+    active
+      ? "text-white border-l-white"
+      : "text-white/50 hover:text-white hover:border-l-white"
+  );
+
+  return (
+    <a className={linkClassName} href={`#${id}`}>
+      {title}
+    </a>
+  );
+};
+
 export const TableOfContent = () => {
+  const [activeHeadlines, setActiveHeadlines] = useState<Array<string>>([]);
   const [headlines, setHeadlines] = useState<Array<HTMLHeadingElement>>([]);
   const pathname = usePathname();
 
@@ -16,16 +41,37 @@ export const TableOfContent = () => {
     setHeadlines(headings);
   }, [pathname]);
 
+  useEffect(() => {
+    const scrollHandler = () => {
+      const scrollTop = window.scrollY;
+      const activeHeadlines: string[] = [];
+
+      headlines.forEach((headline) => {
+        const offsetTop = headline.offsetTop;
+
+        if (scrollTop > offsetTop - window.innerHeight / 2) {
+          activeHeadlines.push(headline.id);
+        }
+      });
+
+      setActiveHeadlines(activeHeadlines);
+    };
+
+    scrollHandler();
+
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [headlines]);
+
   return (
     <div className="grid">
       {headlines.map((headline) => (
         <div key={headline.id}>
-          <a
-            className="text-white/50 hover:text-white text-sm block p-2 border-l border-l-neutral-800 hover:border-l-white transition-colors"
-            href={`#${headline.id}`}
-          >
-            {headline.textContent}
-          </a>
+          <ToCItem
+            id={headline.id}
+            title={headline.textContent ?? ""}
+            active={activeHeadlines.includes(headline.id)}
+          />
         </div>
       ))}
     </div>
